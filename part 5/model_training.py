d75702004b8a8bfed6d7add707aab67cb0439e30
DESKTOP-O0D95C4\user <dgkim1108@gwnu.ac.kr>
2026-01-30 14:38:12 +0900

    no message

---
+++ part 5/model_training.py
@@ -0,0 +1,70 @@
+# step 1.
+from functools import partial
+from kfp.components import create_component_from_func
+import kfp
+from kfp import onprem
+from kfp import compiler
+from kfp import dsl
+from kfp.dsl import component
+import argparse
+import requests
+import asyncio
+import bentoml
+from pydantic import BaseModel
+import typing as t
+import requests
+from kubernetes.client import V1EnvVar, V1Volume, V1VolumeMount, V1EmptyDirVolumeSource
+
+# step 2.
+@partial(create_component_from_func, base_image='dgkim1983/dlabflow:model-20250304')
+def Training(algorithm: str, epochs:int):
+    from ultralytics import YOLO
+    import logging
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s\n\n', datefmt='%Y-%m-%d %H:%M:%S')
+    ch.setFormatter(formatter)
+    kubeflow_logger = logging.getLogger("kubeflow")
+    if not kubeflow_logger.hasHandlers():
+        kubeflow_logger.addHandler(ch)
+    kubeflow_logger.setLevel(logging.INFO)
+    def model_training():
+        model = YOLO(algorithm)
+        results = model.train(data='coco8.yaml', epochs=epochs, project='/mnt/working', name='models', exist_ok=True)
+    try:
+        model_training()
+        kubeflow_logger.info(f"{algorithm} 파일에서 YOLO 모델을 성공적으로 로딩했으며, 이 파일에는 사전 학습된 가중치가 포함되어 있습니다.")
+    except Exception as e:
+        kubeflow_logger.error(f'{algorithm} 파일에서 YOLO 모델 로딩 실패: {e}')
+        raise
+
+# step 3.
+def pipelines():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--algorithm', type=str)
+    parser.add_argument('--epochs', type=int)
+    args = parser.parse_args()
+    Training_task = Training(args.algorithm, args.epochs) \
+        .set_display_name('Model Training') \
+        .apply(onprem.mount_pvc('example-claim', volume_name='data', volume_mount_path='/mnt/working')) \
+        .add_env_variable(V1EnvVar(name="CUDA_VISIBLE_DEVICES", value="0")) \
+        .add_volume(V1Volume(name='dshm', empty_dir=V1EmptyDirVolumeSource(medium='Memory'))) \
+        .add_volume_mount(V1VolumeMount(mount_path='/dev/shm', name='dshm'))
+
+# step 4.
+if __name__ == '__main__':
+    pipeline_package_path = 'training_pipelines.zip'
+    kfp.compiler.Compiler().compile(pipelines, pipeline_package_path)
+    HOST = 'http://10.40.217.244:8080/'
+    USERNAME = 'user@example.com'
+    PASSWORD = '12341234'
+    NAMESPACE = 'kubeflow-user-example-com'
+    session = requests.Session()
+    response = session.get(HOST)
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    data = {'login': USERNAME, 'password': PASSWORD}
+    session.post(response.url, headers = headers, data = data)
+    session_cookie = session.cookies.get_dict()['authservice_session']
+    client = kfp.Client(host = f'{HOST}/pipeline', cookies = f'authservice_session={session_cookie}', namespace = NAMESPACE)
+    experiment = client.create_experiment(name='Training')
+    run = client.run_pipeline(experiment.id, 'Training pipelines', pipeline_package_path)
+
---
+++ part 5/service.py
@@ -0,0 +1,100 @@
+# step 1.
+import shlex
+from pydantic import BaseModel
+from typing import List
+import bentoml
+from bentoml.io import JSON, Image
+from bentoml.exceptions import BentoMLException
+import numpy as np
+import pandas as pd
+from PIL import Image as PILImage
+from datetime import datetime
+import subprocess
+import logging
+import asyncio
+import warnings
+warnings.filterwarnings('ignore')
+
+# step 2.
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s\n\n', datefmt='%Y-%m-%d %H:%M:%S')
+ch.setFormatter(formatter)
+bentoml_logger = logging.getLogger("bentoml")
+if not bentoml_logger.hasHandlers():
+    bentoml_logger.addHandler(ch)
+bentoml_logger.setLevel(logging.INFO)
+
+def log_and_raise_error(message: str, exc: Exception):
+    bentoml_logger.error(f"{message}: {exc}")
+    raise BentoMLException(message)
+
+# step 3.
+class ModelTrainingParams(BaseModel):
+    algorithm: str
+    epochs: int
+
+# step 4.
+svc = bentoml.Service('ai_test')
+
+# step 5.
+@svc.api(input=JSON(), output=JSON(), route='/training')
+async def training(params: dict):
+    name_tag = 'yolo-test'
+    pt_path = '/mnt/working/kubeflow/volumes/models/weights//best.pt'
+    try:
+        parsed_params = ModelTrainingParams(**params)
+        bentoml_logger.info('입력 파라미터가 모두 유효합니다.')
+        algorithm = parsed_params.algorithm
+        epochs = parsed_params.epochs
+        cmd = (
+            f"python3 /mnt/working/kubeflow/pipelines/model_training.py "
+            f"--algorithm={algorithm} "
+            f"--epochs={epochs}"
+        )
+        proc = await asyncio.create_subprocess_exec(
+            *shlex.split(cmd),
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE
+        )
+        stdout, stderr = await proc.communicate()
+        if proc.returncode != 0:
+            log_and_raise_error("모델 훈련 스크립트 실행 실패", stderr.decode())
+        bentoml_logger.info('모델 훈련 스크립트 실행 완료')
+        bentoml_logger.info('모델 훈련 컨테이너를 생성합니다.')
+        with bentoml.models.create(
+            name=name_tag,
+            custom_objects={'pt_path': pt_path},
+            labels={'timestamp': datetime.now().isoformat()}
+        ) as bento_model:
+            bentoml_logger.info(f"BentoML 저장소에서 사용할 수 있는 추론 모델은 {bento_model.tag} 입니다.")
+        models = bentoml.models.list()
+        bentoml_logger.info(f"BentoML 모델 개수: {len(models)}")
+        for model in models:
+            bentoml_logger.info(f"BentoML 모델 이름: {model.tag}, 저장 경로: {model.path}")        
+    except Exception as e:
+        bentoml_logger.error(f"입력 파라미터 파싱 실패: {e}")
+        raise BentoMLException('입력 파라미터가 유효하지 않습니다.')
+
+# step 6.
+@svc.api(input=Image(), output=Image(), route='/inference')
+async def inference(img: PILImage.Image) -> PILImage.Image:
+    from ultralytics import YOLO
+    name_tag = 'yolo-test'
+    try:
+        model_info = bentoml.models.get(name_tag)
+        pt_path = model_info.custom_objects['pt_path']
+        model = YOLO(pt_path)
+        bentoml_logger.info('BentoML 저장소에서 추론 모델을 성공적으로 로딩했습니다.')
+    except Exception as e:
+        bentoml_logger.error(f"BentoML 저장소에서 추론 모델 로딩 실패: {e}")
+        raise BentoMLException('BentoML 저장소에서 추론 모델을 로딩하는데 실패했습니다.')
+    img_np = np.array(img)
+    try:
+        result = model.predict(img_np)
+        result_image = result[0].plot()
+        bentoml_logger.info('모델 추론에 성공했습니다.')
+    except Exception as e:
+        bentoml_logger.error(f"모델 추론 실패: {e}")
+        raise BentoMLException('모델 추론에 실패했습니다.')        
+    return PILImage.fromarray(result_image)
+
